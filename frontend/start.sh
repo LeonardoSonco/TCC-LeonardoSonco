904b83f29c46e5d95db94f874144debdd4525d89
leonardo.sonco <leonardo.sonco@compasso.com.br>
2024-07-30 22:19:41 -0300

    refactor: Refactor and organize code

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-# Função para exibir uma mensagem de erro e sair do script
-function error_exit {
-  echo "$1" 1>&2
-  exit 1
-}
-
-# Caminho do diretório da TCC
-DIRECTORY="./TCC"
-
-# Verifica se o diretório existe
-if [ ! -d "$DIRECTORY" ]; then
-  error_exit "Erro: Diretório $DIRECTORY não encontrado."
-fi
-
 # Entra no diretório da TCC
-cd "$DIRECTORY" || error_exit "Erro: Não foi possível entrar no diretório $DIRECTORY."
+cd ./TCC
 
 # Executa npm install
-npm install || error_exit "Erro: Falha ao executar npm install."
+npm install 
 
 # Executa npm run dev
-npm run dev || error_exit "Erro: Falha ao executar npm run dev."
+npm start 
 
-echo "Script executado com sucesso!"
+
